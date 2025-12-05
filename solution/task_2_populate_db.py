f5ca07b0f5fe18b2004bcd0703790c814e1fb172
cvettkovic <branecvele@gmail.com>
2025-12-05 22:07:41 +0100

    add solution for task 2

---
+++ solution/task_2_populate_db.py
@@ -0,0 +1,22 @@
+import sqlite3 as sql
+import random
+
+
+MIN_RANDOM_NUMBER = 1
+MAX_RANDOM_NUMBER = 20
+
+
+def get_random_number(min_inclusive=MIN_RANDOM_NUMBER, max_inclusive=MAX_RANDOM_NUMBER):
+    return random.randint(min_inclusive, max_inclusive)
+
+
+print(get_random_number()
+      
+      
+      
+      )
+
+
+
+
+
