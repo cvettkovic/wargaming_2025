af72ea0d72c579d0945735f852e6a023b6efe130
cvettkovic <branecvele@gmail.com>
2025-12-09 17:34:23 +0100

    add test file

---
+++ solution/test_data_consistency.py
@@ -0,0 +1,13 @@
+
+
+def test_weapons_table_consistency(original_weapons, temporary_db):
+    pass
+
+
+def test_hulls_table_consistency(original_hulls, temporary_db):
+    pass
+
+
+def test_engines_table_consistency(original_engines, temporary_db):
+    pass
+
